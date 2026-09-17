1aefcb514777bf4f333ec92f9fc35388a37bff78
d-monkey-CG <138995840+d-monkey-CG@users.noreply.github.com>
2026-09-17 18:29:05 -0500

    Refactor settings.py to use dataclass and update variables
    Refactor Settings class to use dataclass features and update variable fetching.

--- Harris-county-elt /dag /settings.py
+++ Harris-county-elt /dag /settings.py
@@ -1,36 +1,38 @@
 import os
 from dataclasses import dataclass
-from datetime import timedelta
-
+from datetime import datetime, timedelta
 from airflow.models import Variable
-from airflow.utils.dates import days_ago
 
-_variables = Variable.get("team_league_elt", deserialize_json=True)
+# Fetch variables using the updated Harris County CAD key
+_variables = Variable.get("harris_county_cad_elt", deserialize_json=True)
 _feature_name = _variables["feature_name"]
 
 
 @dataclass
 class Settings:
-    dag_folder = os.getenv("DAGS_FOLDER")
-    dag_default_args = {
-        'depends_on_past': False,
-        'email': ['airflow@example.com'],
-        'email_on_failure': False,
-        'email_on_retry': False,
-        'retries': 0,
-        'retry_delay': timedelta(minutes=5),
-        "start_date": days_ago(1)
-    }
-    project_id = os.getenv("GCP_PROJECT")
-    queries_path = os.path.join(
-        dag_folder,
-        _feature_name,
-        'dag',
-        'queries'
-    )
+    dag_folder: str = os.getenv("DAGS_FOLDER", "")
+    dag_default_args: dict = None
+    project_id: str = os.getenv("GCP_PROJECT", "")
+    queries_path: str = ""
+    dataset: str = ""
+    hcad_raw_table: str = ""
+    hcad_table: str = ""
+    variables: dict = None
 
-    dataset = _variables["dataset"]
-    team_stat_raw_table = _variables["team_stat_raw_table"]
-    team_stat_table = _variables["team_stat_table"]
-
-    variables = _variables
+    def __post_init__(self):
+        self.dag_default_args = {
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 0,
+            'retry_delay': timedelta(minutes=5),
+            'start_date': datetime(2026, 1, 1),
+        }
+        self.queries_path = os.path.join(
+            self.dag_folder, _feature_name, 'dag', 'queries'
+        )
+        self.dataset = _variables["dataset"]
+        self.hcad_raw_table = _variables["hcad_raw_table"]
+        self.hcad_table = _variables["hcad_table"]
+        self.variables = _variables
